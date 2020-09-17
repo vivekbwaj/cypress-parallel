3d76d9f6f1c1e457b3e80752ec2adbd293b74356
vivek bhardwaj <vivek.bhardwaj@beteasy.com.au>
2020-09-17 16:20:17 +1000

    add screenshots to store as artifacts

--- cypress/integration/test01.spec.js
+++ cypress/integration/test01.spec.js
@@ -2,5 +2,6 @@ describe('Cypress parallel run example - 1', () => {
   it('should display the title', () => {
     cy.visit(`https://mherman.org`);
     cy.get('a').contains('Michael Herman');
+    cy. screenshot();
   });
 });
